43ff75612cc059469dd86be160761132ad0bb7da
Halima <rajwanihalima9@gmail.com>
2023-08-01 12:24:24 +0530

    Calendar Component Made to refactor

---
+++ src/Calendar.jsx
@@ -0,0 +1,100 @@
+import PropTypes from 'prop-types';
+
+const Calendar = ({selectedDate,currentYear,currentMonth,handleDateClick}) => {
+    const hasEventsForDate = (date) => {
+        const eventsForDate = JSON.parse(localStorage.getItem(date)) ;
+        const currentUser = localStorage.getItem('CurrentUser')
+        if(eventsForDate){
+        const userEvents = eventsForDate.filter((event) => event.User == currentUser);
+        return userEvents.length > 0;
+        }else{
+            return false
+        }
+        
+    };
+    
+    const getDaysOfWeek = () => {
+        return ["S", "M", "T", "W", "Th", "F", "S"];
+    }
+    
+    const getDaysInMonth = (year, month) => {
+        return new Date(year, month + 1, 0).getDate();
+    }
+    const getFirstDayOfMonth = (year, month) => {
+        return new Date(year, month, 1).getDay();
+    }
+    const isSameDay = (date1, date2) => {
+        return (
+          date1.getDate() === date2.getDate() &&
+          date1.getMonth() === date2.getMonth() &&
+          date1.getFullYear() === date2.getFullYear()
+        );
+      };
+    
+    const generateCalendarGrid = (year, month) => {
+        const totalDaysInMonth = getDaysInMonth(year, month);
+        const firstDayOfMonth = getFirstDayOfMonth(year, month);
+    
+        const calendarGrid = [];
+        let dayCounter = 1;
+    
+        // Generate rows for the calendar
+        for (let week = 0; week < 6; week++) {
+            const row = [];
+            // Generate cells for each day of the week
+            for (let day = 0; day < 7; day++) {
+                if (
+                    (week === 0 && day < firstDayOfMonth) || dayCounter > totalDaysInMonth) {
+                    row.push(<td key={`${week}-${day}`}></td>);
+                } else {
+                    const date = new Date(year, month, dayCounter);
+                    const isCurrentMonth = date.getMonth() === month;
+                    const classNames = [isCurrentMonth ? 'current-month' : 'other-month'];
+    
+                    if (hasEventsForDate(date)) {
+                        classNames.push('has-events');
+                    }
+                    if (isSameDay(selectedDate, date)) {
+                        classNames.push('selected');
+                      }
+                    row.push(
+                        <td
+                            key={`${week}-${day}`}
+                            className={classNames.join(' ')}
+                            onClick={() => handleDateClick(date)}
+                        >
+                            {dayCounter}
+                        </td>
+                    );
+                    dayCounter++;
+                }
+            }
+            calendarGrid.push(<tr key={week}>{row}</tr>);
+        }
+        return calendarGrid;
+    }
+    
+    
+
+    return (
+        <table>
+                <thead>
+                    <tr>
+                        {getDaysOfWeek().map((day) => (
+                            <th key={day}>{day}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>{generateCalendarGrid(currentYear, currentMonth)}</tbody>
+            </table>
+    )
+}
+Calendar.propTypes = {
+    selectedDate: PropTypes.instanceOf(Date).isRequired,
+    currentYear: PropTypes.number.isRequired,
+    currentMonth: PropTypes.number.isRequired,
+    handleDateClick: PropTypes.func.isRequired,
+    };
+export default Calendar;
+
+
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import EventForm from './EventForm';
 import ConfirmationDialogue from './ConfirmationDialogue';
 import EventsForDate from './EventsForDate'
+import Calendar from './Calendar'
 
 export default function Welcome() {
     const [selectedDate, setSelectedDate] = useState(new Date());
@@ -10,78 +11,7 @@ export default function Welcome() {
     const [showConfirmationDialogue, setShowConfirmationDialogue] = useState(false);
     const [showEventsForDate, setShowEventsForDate] = useState(false);
 
-    const hasEventsForDate = (date) => {
-        const eventsForDate = JSON.parse(localStorage.getItem(date)) ;
-        const currentUser = localStorage.getItem('CurrentUser')
-        if(eventsForDate){
-        const userEvents = eventsForDate.filter((event) => event.User == currentUser);
-        return userEvents.length > 0;
-        }else{
-            return false
-        }
-        
-    };
-
-    const getDaysOfWeek = () => {
-        return ["S", "M", "T", "W", "Th", "F", "S"];
-    }
-
-    const getDaysInMonth = (year, month) => {
-        return new Date(year, month + 1, 0).getDate();
-    }
-    const getFirstDayOfMonth = (year, month) => {
-        return new Date(year, month, 1).getDay();
-    }
-    const isSameDay = (date1, date2) => {
-        return (
-          date1.getDate() === date2.getDate() &&
-          date1.getMonth() === date2.getMonth() &&
-          date1.getFullYear() === date2.getFullYear()
-        );
-      };
-
-    const generateCalendarGrid = (year, month) => {
-        const totalDaysInMonth = getDaysInMonth(year, month);
-        const firstDayOfMonth = getFirstDayOfMonth(year, month);
-
-        const calendarGrid = [];
-        let dayCounter = 1;
-
-        // Generate rows for the calendar
-        for (let week = 0; week < 6; week++) {
-            const row = [];
-            // Generate cells for each day of the week
-            for (let day = 0; day < 7; day++) {
-                if (
-                    (week === 0 && day < firstDayOfMonth) || dayCounter > totalDaysInMonth) {
-                    row.push(<td key={`${week}-${day}`}></td>);
-                } else {
-                    const date = new Date(year, month, dayCounter);
-                    const isCurrentMonth = date.getMonth() === month;
-                    const classNames = [isCurrentMonth ? 'current-month' : 'other-month'];
-
-                    if (hasEventsForDate(date)) {
-                        classNames.push('has-events');
-                    }
-                    if (isSameDay(selectedDate, date)) {
-                        classNames.push('selected');
-                      }
-                    row.push(
-                        <td
-                            key={`${week}-${day}`}
-                            className={classNames.join(' ')}
-                            onClick={() => handleDateClick(date)}
-                        >
-                            {dayCounter}
-                        </td>
-                    );
-                    dayCounter++;
-                }
-            }
-            calendarGrid.push(<tr key={week}>{row}</tr>);
-        }
-        return calendarGrid;
-    }
+    
     const handleDateClick = (date) => {
         setSelectedDate(date);
 
@@ -132,16 +62,7 @@ export default function Welcome() {
                     </button>
                 </h2>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        {getDaysOfWeek().map((day) => (
-                            <th key={day}>{day}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>{generateCalendarGrid(currentYear, currentMonth)}</tbody>
-            </table>
+            <Calendar selectedDate={selectedDate} currentYear={currentYear} currentMonth={currentMonth} handleDateClick={handleDateClick} />
 
         </div>
         <div className="right-sidebar">
